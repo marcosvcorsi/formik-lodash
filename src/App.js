8efdd8e3e543f5dc130d4400028ba92a09f4b2c5
Marcos Corsi <marcosvcorsi@gmail.com>
2020-05-01 15:56:47 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,108 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useRef, useCallback } from 'react';
 import './App.css';
 
+import * as Yup from 'yup';
+import { useFormik } from 'formik';
+import { debounce, throttle } from 'lodash';
+
 function App() {
+  const [textDebounce, setTextDebounce] = useState('');
+  const [textThrottle, setTextThrottle] = useState('');
+
+  const delayedCall = useRef(
+    debounce((value) => {
+      setTextDebounce(value);
+    }, 500)
+  ).current;
+
+  const delayedCallThrottle = useRef(
+    throttle((value) => {
+      setTextThrottle(value);
+    }, 1000)
+  ).current;
+
+  const handleChange = useCallback(
+    (e) => {
+      delayedCall(e.target.value);
+    },
+    [delayedCall]
+  );
+
+  const handleChangeThrottle = useCallback(
+    (e) => {
+      delayedCallThrottle(e.target.value);
+    },
+    [delayedCallThrottle]
+  );
+
+  const onSubmit = useCallback((values) => {
+    alert(JSON.stringify(values));
+  }, []);
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .required('Campo e-mail é obrigatório')
+        .email('E-mail inválido'),
+      password: Yup.string().required('Campo senha é obrigatório'),
+    }),
+
+    onSubmit,
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Debounce after 500ms</h3>
+
+      <div className="side-container">
+        <input placeholder="Debounce" onChange={handleChange} />
+
+        <input placeholder="Result Debonce" value={textDebounce} readOnly />
+      </div>
+
+      <h3>Throttle after 1000ms</h3>
+
+      <div className="side-container">
+        <input placeholder="Throttle" onChange={handleChangeThrottle} />
+
+        <input placeholder="Result Throttle" value={textThrottle} readOnly />
+      </div>
+
+      <form onSubmit={formik.handleSubmit}>
+        <h3>Formik and Yup</h3>
+
+        <input
+          id="email"
+          name="email"
+          placeholder="E-mail"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+
+        {formik.touched.email && formik.errors.email && (
+          <span className="error">{formik.errors.email}</span>
+        )}
+
+        <input
+          id="password"
+          name="password"
+          type="password"
+          placeholder="Senha"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+        />
+
+        {formik.touched.password && formik.errors.password && (
+          <span className="error">{formik.errors.password}</span>
+        )}
+
+        <button type="submit">Enviar</button>
+      </form>
     </div>
   );
 }
